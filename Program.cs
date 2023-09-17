a2545d16f358182e6a32b7f5878ac5d6115b127d
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-17 09:24:58 +0300

    1.lesson

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System; // this is the using statement used to import System library
+namespace OpenGL // this is the namespace declaration for the project
+{
+    class Program // this is the class declaration for the project
+    {
+        static void Main(string[] args) // this is the main method declaration for the project
+        {
+            /*
+            this code is doing the following:
+                1. printing "Hello World!" to the console
+                2. declaring a variable called myname and assigning it a value of "Chris"
+            */
+            Console.WriteLine("Hello World!"); // this is the code that prints "Hello World!" to the console
+        } // this is the end of the main method declaration for the project
+    } // this is the end of the class declaration for the project
+} // this is the end of the namespace declaration for the project
