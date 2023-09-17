cb672b07321e554cf35586569295b44d0853c112
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-17 09:38:19 +0300

    lesson-3

--- Program.cs
+++ Program.cs
@@ -11,7 +11,16 @@ namespace OpenGL // this is the namespace declaration for the project
                 2. printing "Hello " and the value of myname to the console
             */
             string myname = "Chris"; // this is the code that declares a variable called myname and assigns it a value of "Chris"
-            Console.WriteLine("Hello " + myname); // this is the code that prints "Hello " and the value of myname to the console
+
+            /*
+            this is a other example of string interpolation in C#:
+                use + to concatenate strings in C# but it is not recommended
+                use the $ and {variable} syntax instead
+                remember that the $ and {variable} syntax is called string interpolation
+                and with the + syntax it's called string concatenation
+            */
+            Console.WriteLine($"Hello {myname}");
+            
         } // this is the end of the main method declaration for the project
     } // this is the end of the class declaration for the project
 } // this is the end of the namespace declaration for the project
