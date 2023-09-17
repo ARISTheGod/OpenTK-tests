72e8706b4d80fbbebb4efc51d55807878ee3468b
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-17 09:30:22 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,10 +7,9 @@ namespace OpenGL // this is the namespace declaration for the project
         {
             /*
             this code is doing the following:
-                1. printing "Hello World!" to the console
-                2. declaring a variable called myname and assigning it a value of "Chris"
+
             */
-            Console.WriteLine("Hello World!"); // this is the code that prints "Hello World!" to the console
+
         } // this is the end of the main method declaration for the project
     } // this is the end of the class declaration for the project
 } // this is the end of the namespace declaration for the project
