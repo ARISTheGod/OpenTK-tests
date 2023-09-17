082dcb0466e64056a993c6cb6bbe92511894f6fd
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-17 09:27:20 +0300

    lesson-2

--- Program.cs
+++ Program.cs
@@ -7,10 +7,11 @@ namespace OpenGL // this is the namespace declaration for the project
         {
             /*
             this code is doing the following:
-                1. printing "Hello World!" to the console
-                2. declaring a variable called myname and assigning it a value of "Chris"
+                1. declaring a variable called myname and assigning it a value of "Chris"
+                2. printing "Hello " and the value of myname to the console
             */
-            Console.WriteLine("Hello World!"); // this is the code that prints "Hello World!" to the console
+            string myname = "Chris"; // this is the code that declares a variable called myname and assigns it a value of "Chris"
+            Console.WriteLine("Hello " + myname); // this is the code that prints "Hello " and the value of myname to the console
         } // this is the end of the main method declaration for the project
     } // this is the end of the class declaration for the project
 } // this is the end of the namespace declaration for the project
