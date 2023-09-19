5c7e5c6edb5642baed5c81aefc7bdc1b8b940313
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-19 13:49:16 +0300

    lesson-8

--- Program.cs
+++ Program.cs
@@ -7,25 +7,19 @@ namespace OpenGL // this is the namespace declaration for the project
         {
 
             /*
-            let's also Numbers (Integers) Simple arithmetic operations
-
-            instracions:
-                read the comments especially the context comment
+            We can print the limits of the int variable using the MaxValue and MinValue variables, which
+            are variables. With the dot (.) we have access to the methods and also to the variables of the object. The
+            methods such as Trim() above are followed by parentheses while the variable is not.
             */
 
-            int a = 18; // this is the declaration of the variable a with the value of 18
-
-            int b = 6; // the as above but with the variable b and the value of 6
-
-            // this is the declaration of the variable c with the value of a + b
-            int c = a + b; // this should be 24
-
-            Console.WriteLine(c); // this is the output of the variable c to the console this should be 24
-
-            /*
-            context:
-                With the word int we denote whole numbers. The result of C will be 24
-            */
+            // this is the declaration of the max variable
+            int max = int.MaxValue;
+            
+            // this is the declaration of the min variable
+            int min = int.MinValue;
+            
+            // this is the print of the min and max variables
+            Console.WriteLine($"The range of integers is {min} to {max}");
 
         } // this is the end of the main method declaration for the project
     } // this is the end of the class declaration for the project
