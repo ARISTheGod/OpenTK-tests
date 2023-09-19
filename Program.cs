9da2046ccc94943bcf5bb205ae317051f02b3b6c
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-19 13:23:35 +0300

    lesson-5

--- Program.cs
+++ Program.cs
@@ -6,29 +6,23 @@ namespace OpenGL // this is the namespace declaration for the project
         static void Main(string[] args) // this is the main method declaration for the project
         {
             /*
-            this code is doing the following:
-                1. declaring a variable called name and assigning it a value of "Chris"
-                2. declaring a variable called name2 and assigning it a value of "Bill"
+            this will show how to use trim to remove white space from a string
             */
-            string name = "Chris"; // this will declare a variable called name and assign it a value of "Chris"
-            string name2 = "Bill"; // just a second variable called name2 and assign it a value of "Bill"
+
+            // declare a string variable and assign it a value of "      Hello World!       "
+            string greeting = "      Hello World!       ";
+
+            // print to the console the value of the string variable with square brackets around it
+            Console.WriteLine($"[{greeting}]"); // this will print [      Hello World!       ]
 
             /*
-            this is a example of string concatenation in C#
-                the + operator is used to concatenate strings together
-                it's not recommended to use it because it's not as efficient
-                as the string interpolation method
+            this will do the following:
+                1. declare a string variable
+                2. assign it the value of the string variable greeting with the trim method called on it
             */
-            Console.WriteLine("Hello "+ name +" and " + name2); // prints "Hello Chris and Bill" to the console
-
-            /*
-            this is a example of string interpolation in C#
-                the $ operator is used to interpolate strings together
-                it's recommended to use it because it's more efficient
-                than the string concatenation method
-            */
-            Console.WriteLine($"Hello {name} and {name2}"); // prints "Hello Chris and Bill" to the console
+            string trimmedGreeting = greeting.Trim(); // value of trimmedGreeting is "Hello World!"
             
+            Console.WriteLine($"[{trimmedGreeting}]"); // this will print [Hello World!]
         } // this is the end of the main method declaration for the project
     } // this is the end of the class declaration for the project
 } // this is the end of the namespace declaration for the project
