216125f0dd643dbc5c5b11a4a04309aaa01bcf9b
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-19 14:46:12 +0300

    lesson-10

--- Program.cs
+++ Program.cs
@@ -1,5 +1,24 @@
 ﻿using System; // this is the using statement used to import System library
+/*
+Using:
+    The word using is followed by the name of a namespace,
+     a collection of methods and variables, and us allows us to use them in our application.
+    We add our using entries to the top of our program In the example above,
+     we use the System namespace to access the variable
+     Console system with the help of which we write to the console.
+    Alternatively we could write System.Console.WriteLine("Hello World!");
+     and would perform the same function,
+    but this assumes that the namespace we are looking for (System) is known on our computer
+*/
+
 namespace OpenGL // this is the namespace declaration for the project
+/*
+Namespace:
+    The classes we write for our program, we place them inside a namespace,
+     the word namespace – empty – a name of our own – and we surround them with hooks.
+    This helps us organize our programs, they work like folders in a file system.
+    Classes located in the same namespace "see" each other without using the keyword using.
+*/
 {
     class Program // this is the class declaration for the project
     {
@@ -7,34 +26,27 @@ namespace OpenGL // this is the namespace declaration for the project
         {
 
             /*
-            Floating Point Numbers
-
-            Info:
-                The default data type when giving floating point numbers is double.
-
-            More Infos:
-                For money, always decimal. It's why it was created.
-                If numbers must add up correctly or balance, use decimal. This includes any financial storage or calculations, scores, or other numbers that people might do by hand.
-                If the exact value of numbers is not important, use double for speed. This includes graphics, physics or other physical sciences computations where there is already a "number of significant digits".
+            this is using the System namespace to access the variable
+            Console system with the help of which we write to the console.
+            WriteLine is a method that writes a line to the console.
             */
+            System.Console.WriteLine("Hello World!");
 
-            // Double: Variable of type double, or double-precision floating-point number
-            double a = 1.0;
-            double b = 3.0;
-            Console.WriteLine(a / b);
-            // Result: 0.333333333333333
+            /*
+            this is not using the System namespace to access the variable
+            as we have already imported the System namespace at the top of the file
+            by adding the using statement "using System;" to import the System library
+            */
+            Console.WriteLine("Hello World!");
 
-            // Decimal: type variables have more precision than double variables.
-            decimal c = 1.0M;
-            decimal d = 3.0M;
-            Console.WriteLine(c / d);
-            // Result: 0.3333333333333333333333333333
-
-            // Float: Single precision floating point variables, which we use a lot in 3D graphics.
-            float f = 1.0f;
-            float g = 3.0f;
-            Console.WriteLine(f / g);
-            // Result: 0.3333333
+            /*
+            conclusion:
+                we can use the System namespace to access the variable
+                so that we can write to the console without having to write the System
+                this is best practice as it makes the code more readable
+                but sometimes we may want to write the System to make it clear
+                e.g. if we have a variable called Console in our code
+            */
 
         } // this is the end of the main method declaration for the project
     } // this is the end of the class declaration for the project
