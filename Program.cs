30bb78fc76f7ed55152c901a6707bb1a4e2e09e0
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-19 18:37:58 +0300

    lesson-13

--- Program.cs
+++ Program.cs
@@ -1,60 +1,44 @@
 ﻿using System;
+
 namespace OpenGL
 {
-
-    public class Car
-    {
-        int doors; //how many doors does the car have?
-        string color; //what color is the car
-        double topspeed; //the top speed of the car
-
-        public Car (int d, string c, double t) 
-        { 
-            doors = d;
-            color = c;
-            topspeed = t;
-        }
-
-        /*
-        2.
-            We can declare color public: public string color; and printing will succeed.
-            But the best case for variables is to make set/get methods to change/read them respectively.
-            
-            Then we write: Console.WriteLine(mycar.GetColor()); //pay attention to parentheses.
-        */
-        public string GetColor() // color is of type string
-        {
-            return color; // the word return indicates that GetColor will return the color
-        }
-    }
 
     class Program
     {
         static void Main() // this is the main method declaration for the project
         {
+        
+        /*
+        Random numbers
+            As a practice on objects and variables, we will make random numbers
+        */
 
-            // is the car mycar with 4 doors, white color and top speed 300
-            Car mycar = new Car(4, "white", 300);
+        int selection; // Create variable (integer)
 
-            /*
-            1.
-            Variable / method visibility:
-                Which variables are visible when we use the dot depends on whether we have declared them public or private.
-                THE word public indicates visibility, word private hides variables and methods.
-                Variables by default are private that is, they are hidden.
-                For example if we write: Console.WriteLine(mycar.color);
-                Printing will fail because with the dot we will not have access to the color variable because it is private.
-            */
-            // you can try it out by uncommenting the line below the printing will fail
-            // Console.WriteLine(mycar.color);
+        /*
+        The Random class is used to create random numbers
+        */
+        Random rand = new Random(); // Create an object of type Random
 
-            /*
-            3.
-            testing the GetColor method
-            */
-            Console.WriteLine(mycar.GetColor());
+        /*
+        The Next method returns a random number between the two parameters we specified, 
+         in this case it will be a is a number between 1 and 7.
+        */
+        selection = rand.Next(1, 7); // call the Next method giving 1.7 as parameters
 
-            // conclusion: we can access the color variable through the GetColor method
+        // Print the random number
+        Console.WriteLine("You rolled a " + selection);
+
+        /*
+        conclusion: the Next method of the Random class is used to generate random numbers
+        it's important to note that the Next method is overloaded, meaning that it can be called
+        with different parameters to generate different types of random numbers
+
+        Info:
+            but it's not secure, it's not cryptographically secure, meaning that it's not secure enough
+            to be used in cryptography, for example, to generate passwords or to generate keys for encryption.
+        */
+
         }
     }
 
