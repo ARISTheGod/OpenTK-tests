603348b601fc92a7e5144c57cb18e15e18c0a347
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-19 14:21:29 +0300

    lesson-9

--- Program.cs
+++ Program.cs
@@ -7,19 +7,34 @@ namespace OpenGL // this is the namespace declaration for the project
         {
 
             /*
-            We can print the limits of the int variable using the MaxValue and MinValue variables, which
-            are variables. With the dot (.) we have access to the methods and also to the variables of the object. The
-            methods such as Trim() above are followed by parentheses while the variable is not.
+            Floating Point Numbers
+
+            Info:
+                The default data type when giving floating point numbers is double.
+
+            More Infos:
+                For money, always decimal. It's why it was created.
+                If numbers must add up correctly or balance, use decimal. This includes any financial storage or calculations, scores, or other numbers that people might do by hand.
+                If the exact value of numbers is not important, use double for speed. This includes graphics, physics or other physical sciences computations where there is already a "number of significant digits".
             */
 
-            // this is the declaration of the max variable
-            int max = int.MaxValue;
-            
-            // this is the declaration of the min variable
-            int min = int.MinValue;
-            
-            // this is the print of the min and max variables
-            Console.WriteLine($"The range of integers is {min} to {max}");
+            // Double: Variable of type double, or double-precision floating-point number
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+            // Result: 0.333333333333333
+
+            // Decimal: type variables have more precision than double variables.
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+            // Result: 0.3333333333333333333333333333
+
+            // Float: Single precision floating point variables, which we use a lot in 3D graphics.
+            float f = 1.0f;
+            float g = 3.0f;
+            Console.WriteLine(f / g);
+            // Result: 0.3333333
 
         } // this is the end of the main method declaration for the project
     } // this is the end of the class declaration for the project
