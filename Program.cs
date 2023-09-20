c194eeb265bbd734284183067266bbae6256c2f1
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-20 18:06:28 +0300

    lesson-14

--- Program.cs
+++ Program.cs
@@ -7,37 +7,50 @@ namespace OpenGL
     {
         static void Main() // this is the main method declaration for the project
         {
-        
-        /*
-        Random numbers
-            As a practice on objects and variables, we will make random numbers
-        */
 
-        int selection; // Create variable (integer)
+            /*
+            Program flow
+                To change the flow of the program we use the structure if(condition1)...else if(condition2)...else
+            */
 
-        /*
-        The Random class is used to create random numbers
-        */
-        Random rand = new Random(); // Create an object of type Random
+            int selection; // Create variable (integer)
+            Random rand = new Random(); // Create an object of type Random
 
-        /*
-        The Next method returns a random number between the two parameters we specified, 
-         in this case it will be a is a number between 1 and 7.
-        */
-        selection = rand.Next(1, 7); // call the Next method giving 1.7 as parameters
+            /*
+            The Next method returns a random number between the two parameters we specified,
+             in this case it will is a number between 1 and 7.
+            */
+            selection = rand.Next(1, 7); // call the Next method giving 1.7 as parameters
 
-        // Print the random number
-        Console.WriteLine("You rolled a " + selection);
+            /*
+            In the random number example above,
+             to print the day of the week that corresponds to number we can use the if structure as follows:
+            */
+            if (selection == 1) /*
+                    in the condition we use double = because single is for entering value.
+                    Here we ask "is selection equal to 1?" If YES then execution proceeds inside the brackets,
+                    otherwise it goes to else if
+                */
+            {
+                Console.WriteLine("Monday");
+            }
+            else if (selection == 2)
+            {
+                Console.WriteLine("Tuesday");
+            }
+            else if (selection == 7)
+            {
+                Console.WriteLine("Sunday");
+            }
 
-        /*
-        conclusion: the Next method of the Random class is used to generate random numbers
-        it's important to note that the Next method is overloaded, meaning that it can be called
-        with different parameters to generate different types of random numbers
-
-        Info:
-            but it's not secure, it's not cryptographically secure, meaning that it's not secure enough
-            to be used in cryptography, for example, to generate passwords or to generate keys for encryption.
-        */
+            /*
+            Once all the else ifs are finished the execution ends.
+            If we want something to be done if the number does not exist in any condition
+             then we finish by writing plain else. For example, if for a random number we wrote:
+            selection = rand.Next(1, 10); It would return numbers from 1 to 10. We would write the code as above and be done
+            writing:
+            } else { Console.WriteLine(“Does not match day!”); } because numbers over 7 do not correspond to a day week
+            */
 
         }
     }
