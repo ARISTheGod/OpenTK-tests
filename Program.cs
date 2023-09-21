7fc318aef9e5926978907f1b59ef1186b71b14c4
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-21 11:25:39 +0300

    lesson-16

--- Program.cs
+++ Program.cs
@@ -1,56 +1,63 @@
 ﻿using System;
-
 namespace OpenGL
 {
+    public class Car
+    {
+        int doors; //how many doors does the car have?
+        string color; //what color is the car
+        double topspeed; //the top speed of the car
+
+        public Car (int d, string c, double t) 
+        { 
+            doors = d;
+            color = c;
+            topspeed = t;
+        }
+        public void Start() // Start is a method of the Car class
+        {
+            // example of a method for starting a car
+            Console.WriteLine("The car is starting");
+        }
+        public string GetColor() // color is of type string
+        {
+            return color; // the word return indicates that GetColor will return the color
+        }
+    }
+    /*
+
+    Heredity
+        By inheritance we mean that an object type (child)
+        can simultaneously be a type of one of another object (parent)
+        
+        In the car example, let's say we have a class named Sportscar, which will declare a different one
+        type of car
+    */
+    public class SportsCar : Car
+    //We use the colon to indicate that Sportscar is also Car.
+    {
+        //SportsCar has a new variable called model (the model of the car)
+        String model;
+        
+        // The car constructor is as follows: public Car (int d, string c, double t)
+        public SportsCar(int d, string c, double t, string m):base(d, c, t)
+
+        // We write a colon and then the word base to call the constructor of the "parent" of the SportsCar
+        // i.e. the Car
+        {
+            model=m;
+        }
+    }
 
     class Program
     {
         static void Main() // this is the main method declaration for the project
         {
 
-            /*
-            Program flow
-                Another way to do the same is by using the Switch structure
-            */
-
-            /*
-            are random number generator to select a number between 1 and 7
-            */
-            int selection; // we declare the variable selection as an integer
-            Random rand = new Random(); // we declare the variable rand as a random number generator object
-            selection = rand.Next(1,7); /*
-            we use the method Next of the object rand to select a number between 1 and 7
-            */
-
-            switch (selection) // we start by putting in parentheses the variable we will check
-            {
-            case 1: // with the word case we state what the selection will be equal to
-                Console.WriteLine("Monday"); 
-                break; /*
-                at the end we write break to stop the execution.
-                Without it the code will continue below (in Case 2)
-                and so on until it finds a break or the end of the switch.
-                most of the times we use break to avoid errors and to make the code more readable
-                */
-            case 2:
-                Console.WriteLine("Tuesday");
-                break;
-            case 3:
-                Console.WriteLine("Wednesday");
-                break;
-            case 4:
-            Console.WriteLine("Thursday");
-                break;
-            case 5:
-                Console.WriteLine("Friday");
-                break;
-            case 6:
-                Console.WriteLine("Saturday");
-                break;
-            case 7:
-                Console.WriteLine("Sunday ");
-                break;
-            }
+            // So a SportsCar variable can access Car variables and methods
+            SportsCar SC = new SportsCar(2, "white", 500, "Ferrari");
+            /// the first 3 parameters are for Car, and one for SportsCar
+            
+            SC.Start(); //run the Start method of Car
 
         }
     }
