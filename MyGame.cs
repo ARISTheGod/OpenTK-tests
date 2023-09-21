ffe7c14207feee6e86558fdcadf062619e3b9f31
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-21 18:43:19 +0300

    lesson-19

--- MyGame.cs
+++ MyGame.cs
@@ -3,6 +3,7 @@ using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Mathematics;
+using System.Drawing;
 
 namespace OpenGL1
 {
@@ -16,18 +17,28 @@ namespace OpenGL1
 
         public MyGame() : base(GameWindowSettings.Default, NativeWindowSettings.Default)
         {
-            
+            this.CenterWindow(
+                new Vector2i(1280, 768)
+            ); /*
+                this line centers the application window on the screen, the parameter is the size of the window
+                in pixels, we can also use the size of the screen, but it is not recommended as it can be
+                too big for some screens.
+            */
         }
 
-        // /*
-        // OnLoad
-        //     In OnLoad we write code that loads audio, image files and 3D models into memory, it runs
-        //     once at the beginning of the program
-        // */
-        // protected override void OnLoad(EventArgs e)
-        // {
-
-        // }
+        /*
+        OnLoad
+            In OnLoad we write code that loads audio, image files and 3D models into memory, it runs
+            once at the beginning of the program
+        */
+        protected override void OnLoad()
+        {
+            GL.ClearColor(Color.SteelBlue); /*
+            the firt on the lodeing of the application, we set the background color of the application window
+            will be steel blue, we can use any color we want, but we need to use the Color class to do this.
+            best practice is to use Coler4 class, but we can use Color class too as it is more simple.
+            */
+        }
 
         /*
         OnRenderFrame:
@@ -35,6 +46,8 @@ namespace OpenGL1
         */
         protected override void OnRenderFrame(FrameEventArgs e)
         {
+            this.Context.SwapBuffers(); // swap the front and back buffers of the window
+
             /*
             this is the method that draws the graphics on the screen, it is called every time
             that the application window is updated, that is, every time OnUpdateFrame is called.
@@ -49,6 +62,7 @@ namespace OpenGL1
                 This line is necessary to run the application
                 as the default OnRenderFrame method of GameWindow is empty, we need to call the base method
             */
+
         }
 
         /*
