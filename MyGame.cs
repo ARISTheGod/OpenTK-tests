8043bd5e3fe116da17419e2f67efa0d7d39fd957
aris <50799554+ARISTheGod@users.noreply.github.com>
2023-09-21 13:07:46 +0300

    lesson-17

---
+++ MyGame.cs
@@ -0,0 +1,46 @@
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Windowing.Desktop;
+
+namespace OpenGL1
+{
+    public class MyGame : GameWindow
+    {
+
+        /*
+        The main body of an OpenGL application, except for the namespace name, which changes, the other parts will
+        we use in each of our applications:
+        */
+
+        /*
+        OnLoad
+            In OnLoad we write code that loads audio, image files and 3D models into memory, it runs
+            once at the beginning of the program
+        */
+        protected override void OnLoad(EventArgs e)
+        {
+
+        }
+
+        /*
+        OnRenderFrame
+            This method is responsible for displaying the graphics of the application on the screen
+        */
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+
+        }
+
+        /*
+        OnUpdateFrame
+            This method runs when we want to update the application window, the one we placed as
+            parameter in the Run method or as long as the hardware can withstand if we didn't put something. Update is where we put the logic
+            and movement in the application.
+        */
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,65 +1,58 @@
-﻿using System;
-namespace OpenGL
+﻿namespace OpenGL1
 {
-    public class Car
-    {
-        int doors; //how many doors does the car have?
-        string color; //what color is the car
-        double topspeed; //the top speed of the car
-
-        public Car (int d, string c, double t) 
-        { 
-            doors = d;
-            color = c;
-            topspeed = t;
-        }
-        public void Start() // Start is a method of the Car class
-        {
-            // example of a method for starting a car
-            Console.WriteLine("The car is starting");
-        }
-        public string GetColor() // color is of type string
-        {
-            return color; // the word return indicates that GetColor will return the color
-        }
-    }
-    /*
-
-    Heredity
-        By inheritance we mean that an object type (child)
-        can simultaneously be a type of one of another object (parent)
-        
-        In the car example, let's say we have a class named Sportscar, which will declare a different one
-        type of car
-    */
-    public class SportsCar : Car
-    //We use the colon to indicate that Sportscar is also Car.
-    {
-        //SportsCar has a new variable called model (the model of the car)
-        String model;
-        
-        // The car constructor is as follows: public Car (int d, string c, double t)
-        public SportsCar(int d, string c, double t, string m):base(d, c, t)
-
-        // We write a colon and then the word base to call the constructor of the "parent" of the SportsCar
-        // i.e. the Car
-        {
-            model=m;
-        }
-    }
-
     class Program
     {
-        static void Main() // this is the main method declaration for the project
+        /* 2. Program.cs
+        In the Program.cs file is the Main method which is the first method executed when we run it
+        our program. Its only task is to create an object (named game here) of type MyGame and
+        then execute the Run method of that object.
+        */
+        static void Main(string[] args)
         {
+            /* 1. Introduction
+            OpenGL
+            OpenGL is an API (Application Programming Interface) a set of functions and methods that
+            they help to display 2D and 3D graphics on the screen.
+            While we call OpenGL a programming language, we cannot actually program
+            directly in OpenGL but we need another programming language (like C#)
+            Actually OpenGL is a specification, it defines what effect the functions should have
+            her. However, the graphics card manufacturers are responsible for implementing these functions,
+            AMD, INTEL, NVIDIA. In other words, OpenGL itself does not offer the content of the functions but only the ones
+            names, parameters and gives information about exactly what these functions are supposed to do.
 
-            // So a SportsCar variable can access Car variables and methods
-            SportsCar SC = new SportsCar(2, "white", 500, "Ferrari");
-            /// the first 3 parameters are for Car, and one for SportsCar
-            
-            SC.Start(); //run the Start method of Car
+            OpenTK
+            There are many libraries that allow us to use OpenGL functions, the library that
+            is more compatible with C# and is in constant development (others have been stopped for years) is OpenTK
+            The first step when starting an OpenGL application is to open a window. I will
+            make a class, which can be in the same file or in a different one, but always in the same namespace.
+            */
 
+
+            /* 3. MyGame 
+            The game is a copy of the MyGame class that is loaded into memory with some default values, which
+            we can change. We can have many different objects of the same type even if we change
+            values in one, will not change in the others.
+            MyGame.cs file
+
+            After the name, the colon indicates that the MyGame class will inherit from the GameWindow class.
+            Notice that the Run method we call on the game object has not been implemented by us (the
+            MyGame.cs is practically empty) the Run method is part of GameWindow, and since MyGame is
+            at the same time GameWindow you have all the methods and variables of GameWindow.
+            All windows in OpenGL, after the creation of the object, are started with the Run() method which like
+            parameter accepts the frame rate (frames per second) i.e. how fast the application will refresh (such as
+            we'll see below, whenever OnUpdateFrame will run), if we leave it blank then our program will run
+            as fast as our hardware can handle it.
+            The 3 main methods we need to implement in an OpenGL application are:
+            OnLoad
+            OnRenderFrame
+            OnUpdateFrame
+            There are other methods but these 3 are the main ones that are present in almost every program/application.
+            These methods are in the GameWindow class, to use them we need to change them
+            their contents to do the tasks we want. We achieve this by writing the word override, where
+            will tell the compiler that we don't want GameWindow's OnLoad but our own.
+            */
+            MyGame game = new MyGame();
+            game.Run();
         }
     }
-
 }
